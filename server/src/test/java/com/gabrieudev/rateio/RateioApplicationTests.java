cf1b5b573778986f4d16c55a4a863f66ebb19c17
gabrieudev <joao.gabriel@atoscapital.com.br>
2026-02-19 10:38:50 -0300

    chore(server): iniciar servidor

---
+++ server/src/test/java/com/gabrieudev/rateio/RateioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieudev.rateio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
